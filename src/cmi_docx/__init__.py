aa5cd92342171c9fa00554bea20d2e91457c41ca
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-12-18 12:30:22 -0500

    feat: Add comment functionality (#33)

--- src/cmi_docx/__init__.py
+++ src/cmi_docx/__init__.py
@@ -1,5 +1,6 @@
 """.. include:: ../../README.md"""  # noqa: D415
 
+from cmi_docx.comment import add_comment  # noqa: F401
 from cmi_docx.document import ExtendDocument  # noqa: F401
 from cmi_docx.paragraph import ExtendParagraph, FindParagraph  # noqa: F401
 from cmi_docx.run import ExtendRun, FindRun  # noqa: F401
